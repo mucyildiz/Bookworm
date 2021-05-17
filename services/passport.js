f43094fcd29caf7c90c2088b1b2145026cbf6c72
Alperen Yildiz <alperenyildiz21@gmail.com>
2021-05-17 01:11:19 -0400

    added serializeUser and deserializeUser for future use with passport OAuth and mongoDB

---
+++ services/passport.js
@@ -0,0 +1,18 @@
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const keys = require('../config/keys');
+const mongoose = require('mongoose');
+
+const User = mongoose.model('users');
+
+passport.serializeUser((user, done) => {
+  done(null, user.id);
+});
+
+passport.deserializeUser((id, done) => {
+  User.findById(id)
+    .then(user => {
+      done(null, user);
+    })
+});
+
