413ae63e11c6bb597c7048fdf119f4f87193b4ba
Alperen Yildiz <alperenyildiz21@gmail.com>
2021-05-17 20:30:01 -0400

    added authRoutes to index.js along with passport initialization in index.js

--- index.js
+++ index.js
@@ -1,11 +1,19 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const keys = require('./config/keys');
+const passport = require('passport');
+require('./models/User');
+require('./services/passport');
 
 const mongoURI = keys.mongoURI;
 mongoose.connect(mongoURI, {useNewUrlParser: true, useUnifiedTopology: true});
 
 const app = express();
 
+require('./routes/authRoutes')(app);
+
+app.use(passport.initialize());
+app.use(passport.session());
+
 const PORT = process.env.PORT || 5000;
-app.listen(PORT);
+app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
