ce119337110265f2a2964589f303e8773dcd73dd
Alperen Yildiz <alperenyildiz21@gmail.com>
2021-05-18 20:50:49 -0400

    made components directory with Header and App components

---
+++ client/src/components/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import Header from './Header';
+
+function App() {
+  return (
+    <div id='app-container'>
+      <Header />
+    </div>
+  );
+}
+
+export default App;
---
+++ client/src/components/Header.js
@@ -0,0 +1,18 @@
+import './Header.css';
+
+function Header() {
+  return (
+    <div id='header-container'>
+      <div id='header'>
+        <div id='title'>
+          Bibliophili
+        </div>
+        <div id='user-options'>
+          <div id='login'>Login</div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Header;
