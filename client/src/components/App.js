b51880a8a4a2200c8360d07fe768aa37423f30a8
Alperen Yildiz <alperenyildiz21@gmail.com>
2021-05-23 13:44:49 -0400

    added react-router with routes in App

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,12 +1,21 @@
 import './App.css';
+import { BrowserRouter, Route } from 'react-router-dom';
 import Header from './Header';
 import Home from './Home';
+import Library from './Library';
+import Recommendations from './Recommendations';
 
 function App() {
   return (
     <div id='app-container'>
-      <Header />
-      <Home />
+      <BrowserRouter>
+        <div>
+          <Header />
+          <Route exact path="/" component={Home} />
+          <Route exact path="/my-books" component={Library} />
+          <Route exact path='/recommendations' component={Recommendations} />
+        </div>
+      </BrowserRouter>
     </div>
   );
 }
