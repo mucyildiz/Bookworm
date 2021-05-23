0978771449a9f659c6ee8f3126f0b6623c7055d7
Alperen Yildiz <alperenyildiz21@gmail.com>
2021-05-23 13:01:56 -0400

    added proper html outline and tabs for header

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -3,10 +3,17 @@ import './Header.css';
 function Header() {
   return (
     <div id='header-container'>
-      <div id='header'>
+      <div id='header-left'>
         <div id='title'>
           Bibliophili
         </div>
+        <div id='header-tabs'>
+          <div className='header-tab'>Home</div>
+          <div className='header-tab'>My Books</div>
+          <div className='header-tab'>Get Recommendations</div>
+        </div>
+      </div>
+      <div id='header-right'>
         <div id='user-options'>
           <div id='login'>Login</div>
         </div>
