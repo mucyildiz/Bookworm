23523543ee954c545ed027e75cdf17b3a85e6387
Alperen Yildiz <alperenyildiz21@gmail.com>
2021-05-23 13:00:34 -0400

    added Home component to App

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import Header from './Header';
+import Home from './Home';
 
 function App() {
   return (
     <div id='app-container'>
       <Header />
+      <Home />
     </div>
   );
 }
