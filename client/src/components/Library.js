910117c7ea4fa0b7d06b4f59e5ffa1253c4d4145
Alperen Yildiz <alperenyildiz21@gmail.com>
2021-05-28 17:09:49 -0400

    initialized library and recommendations pages

---
+++ client/src/components/Library.js
@@ -0,0 +1,13 @@
+import './Header.css';
+
+function Library() {
+  return (
+    <div id='container'>
+      <div id='add-book'>
+        Test
+      </div>
+    </div>
+  )
+}
+
+export default Library;
---
+++ client/src/components/Recommendations.js
@@ -0,0 +1,10 @@
+import './Recommendations.css';
+function Recommendations() {
+  return (
+    <div id='container'>
+      <div>Recommendations</div>
+    </div>
+  )
+}
+
+export default Recommendations;
