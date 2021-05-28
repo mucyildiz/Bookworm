81a81e04b9de27330c8112cb9e08bc11b8964574
Alperen Yildiz <alperenyildiz21@gmail.com>
2021-05-28 17:48:36 -0400

    added redirect to home page after signing in with Google

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -10,6 +10,9 @@ module.exports = app => {
 
   app.get(
     '/auth/google/callback',
-    passport.authenticate('google')
+    passport.authenticate('google', {
+      successRedirect: process.env.NODE_ENV === 'production' ? '/' : 'http://localhost:3000/',
+      failureRedirect: process.env.NODE_ENV === 'production' ? "/" : "http://localhost:3000/"
+    })
   );
 }
