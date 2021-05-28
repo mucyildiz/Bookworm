09c6595304555fe8d2c9da2d30f2a4442234367b
Alperen Yildiz <alperenyildiz21@gmail.com>
2021-05-28 17:48:13 -0400

    added authRoutes intiliztion after passport initialization

--- index.js
+++ index.js
@@ -10,10 +10,10 @@ mongoose.connect(mongoURI, {useNewUrlParser: true, useUnifiedTopology: true});
 
 const app = express();
 
-require('./routes/authRoutes')(app);
-
 app.use(passport.initialize());
 app.use(passport.session());
+
+require('./routes/authRoutes')(app);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
