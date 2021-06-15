09f8d29647cf7d8fd6695d1cc782f63fec065290
Alperen Yildiz <alperenyildiz21@gmail.com>
2021-06-15 19:49:47 -0400

    make api call before component mounts to get user first name and db id

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,11 +1,27 @@
 import './Home.css';
 import { Link } from 'react-router-dom';
+import { useState, useEffect } from 'react';
+import axios from 'axios';
 
 function Home() {
+  const [user, setUser] = useState({});
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const response = await axios.get('/api/getuser');
+      const userInfo = await response.data;
+      setUser({
+        firstName: userInfo.firstName,
+        id: userInfo._id
+      });
+    };
+    fetchData();
+  }, [])
+
   return (
     <div id='home-container'>
       <div id='greeting'>
-        <div>Welcome Name!</div>
+        <div>Welcome {user.firstName}!</div>
         <div>You have x books in your library.</div>
       </div>
       <div id='options'>
