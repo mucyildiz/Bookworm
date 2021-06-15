0ba1361214a725268b5697aa7e2ed3c9269395dd
Alperen Yildiz <alperenyildiz21@gmail.com>
2021-06-15 19:52:38 -0400

    added /api/getuser endpoint to obtain user id and firstname from backed

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,6 +1,8 @@
 const passport = require('passport');
 
 module.exports = app => {
+  let userInfo = {};
+
   app.get(
     '/auth/google',
     passport.authenticate('google', {
@@ -8,11 +10,15 @@ module.exports = app => {
     })
   );
 
-  app.get(
-    '/auth/google/callback',
-    passport.authenticate('google', {
-      successRedirect: process.env.NODE_ENV === 'production' ? '/' : 'http://localhost:3000/',
-      failureRedirect: process.env.NODE_ENV === 'production' ? "/" : "http://localhost:3000/"
-    })
-  );
+  app.get('/auth/google/callback', (req, res) => {
+  passport.authenticate('google', (err, user) => {
+    userInfo = {
+      firstName: user.firstName,
+      _id: user._id
+    };
+    return res.redirect(process.env.NODE_ENV === 'production' ? '/' : 'http://localhost:3000/');
+  })(req, res);
+  });
+
+  app.get('/api/getuser', (req, res) => {res.json(userInfo)});
 }
