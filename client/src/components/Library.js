e51575edfb793e9ea44ae4845240d34b3bc02491
Alperen Yildiz <alperenyildiz21@gmail.com>
2021-06-16 16:19:56 -0400

    Added searchbar to library component

--- client/src/components/Library.js
+++ client/src/components/Library.js
@@ -1,19 +1,19 @@
-import './Header.css';
-import { useState, useEffect } from 'react';
+import './Library.css';
+import Search from './Search';
 import axios from 'axios';
 
 const Library = (props) => {
   const googleBooksAPIKey = props.apiKey;
+
   const test = async () => {
     const response = await axios.get(`https://www.googleapis.com/books/v1/volumes?q=harry-potter&key=AIzaSyDYLvzacyDGmqhxYbqSFBLGa1F3OYY_cbU`);
     const data = await response.data;
     console.log(data.items);
   }
+
   return (
-    <div id='container'>
-      <div id='add-book'>
-        <div onClick={test}>Here</div>
-      </div>
+    <div id='library-container'>
+      <Search id='search-bar' key={googleBooksAPIKey} />
     </div>
   )
 }
