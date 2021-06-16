e5df1e9565576058e45e459d46c40cd00b4ac205
Alperen Yildiz <alperenyildiz21@gmail.com>
2021-06-16 19:14:21 -0400

    added search bar with books popping up as user searches

--- client/src/components/Search.js
+++ client/src/components/Search.js
@@ -1,14 +1,67 @@
+import { useState } from 'react';
+import axios from 'axios';
+import BookResult from './BookResult';
+import { pick } from 'lodash';
+
 const Search = (props) => {
+  const [searchQuery, setSearchQuery] = useState('');
+  const [isLoading, setIsLoading] = useState(false);
+  const [bookResults, setBookResults] = useState([])
+
+  const getResults = async (query) => {
+    setIsLoading(true);
+    const res = await axios.get(`https://www.googleapis.com/books/v1/volumes?q=${query}&key=${props.googleAPIKey}`)
+    const data = await res.data;
+    const books = data.items.map(book => (
+      pick(book, ['id', 'volumeInfo'])
+    ));
+    setIsLoading(false);
+    setBookResults(books);
+  }
+
+  const handleInput = async e => {
+    setSearchQuery(e.target.value);
+    
+    if(searchQuery.length > 1){
+      await getResults(searchQuery);
+    }
+    // make books empty because when user starts searching again after clearing query itll show results from last search
+    else{
+      setBookResults([]);
+    }
+  }
+
   return (
+    <>
     <form action='/' method='GET'>
         <input
+        value={searchQuery}
+        onInput={handleInput}
         type="text"
-        id="header-search"
+        id="book-search"
         placeholder="Search books"
-        name="s" 
+        name="search" 
         />
         <button type="submit">Search</button>
     </form>
+    <div id='results'>
+      {bookResults &&
+      <ul>
+          {bookResults.map(book => (
+            <li key={book.id}>
+              <BookResult 
+              bookId={book.id}
+              title={book.volumeInfo.title}
+              subtitle={book.volumeInfo.subtitle}
+              imgUrl={book.volumeInfo.imageLinks ? book.volumeInfo.imageLinks.smallThumbnail : '/images/placeholderbook.svg'}
+              author={book.volumeInfo.authors ? book.volumeInfo.authors.join('') : ''}
+              />
+            </li>
+          ))}
+      </ul>
+      }
+    </div>
+    </>
   )
 }
 
