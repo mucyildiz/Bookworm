a972069d00449511f1ae7faf8c4dc3e8b204ce27
Alperen Yildiz <alperenyildiz21@gmail.com>
2021-06-16 02:07:42 -0400

    header is rendered conditionally based off whether user logged in

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,17 +1,15 @@
 import './Header.css';
 import { Link } from 'react-router-dom';
 
-function Header() {
+function Header(props) {
   return (
     <div id='header-container'>
       <div id='header-left'>
-        <div id='title'>
-          Bibliophili
-        </div>
+        <Link to='/'>
+        <div id='title'>Bibliophili</div>
+        </Link>
+        {props.isAuthed && 
         <div id='header-tabs'>
-          <Link to='/'>
-            <div className='header-tab'>Home</div>
-          </Link>
           <Link to='/my-books'>
             <div className='header-tab'>My Books</div>
           </Link>
@@ -19,10 +17,17 @@ function Header() {
             <div className='header-tab'>Get Recommendations</div>
           </Link>
         </div>
+        }
       </div>
       <div id='header-right'>
         <div id='user-options'>
           <div className='btn'>
+            {props.isAuthed ? 
+            <a href='/api/logout'>
+              <div id='login-content'>
+                <span>Log out</span>
+              </div>
+            </a> :
             <a href='/auth/google'>
               <div id='login-content'>
                 <img className='google-logo' src='https://upload.wikimedia.org/wikipedia/commons/5/53/Google_%22G%22_Logo.svg' alt='' />
@@ -31,6 +36,7 @@ function Header() {
                 </span>
               </div>
             </a>
+            }
           </div>
         </div>
       </div>
