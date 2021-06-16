2ef600a42fbb5a0ed02750f80d42580773635047
Alperen Yildiz <alperenyildiz21@gmail.com>
2021-06-15 23:29:23 -0400

    added cookieSession and configured

--- index.js
+++ index.js
@@ -2,20 +2,26 @@ const express = require('express');
 const mongoose = require('mongoose');
 const keys = require('./config/keys');
 const passport = require('passport');
+const cookieSession = require('cookie-session');
 require('./models/User');
 require('./services/passport');
-
-const app = express();
 
 const mongoURI = keys.mongoURI;
 mongoose.connect(mongoURI, {useNewUrlParser: true, useUnifiedTopology: true});
 
+const app = express();
 
-
+app.use(
+  cookieSession({
+      maxAge: 30 * 24 * 60 * 60 * 1000, //30 days in ms
+      keys: [keys.cookieKey]
+  })
+)
 app.use(passport.initialize());
 app.use(passport.session());
 
 require('./routes/authRoutes')(app);
+require('./routes/userRoutes')(app);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
