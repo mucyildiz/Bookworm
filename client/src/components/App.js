4f7874f78d42ca7130448c700df10248c4b2dcaa
Alperen Yildiz <alperenyildiz21@gmail.com>
2021-06-16 02:07:24 -0400

    added api call to get user to App.js

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -4,14 +4,30 @@ import Header from './Header';
 import Home from './Home';
 import Library from './Library';
 import Recommendations from './Recommendations';
+import { useState, useEffect } from 'react';
+import axios from 'axios';
 
 function App() {
+  const [userName, setUserName] = useState('');
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const response = await axios.get('/api/getuser');
+      const userInfo = await response.data;
+      setUserName(userInfo.firstName);
+    };
+    fetchData();
+  }, [])
+
   return (
     <div id='app-container'>
       <BrowserRouter>
         <div>
-          <Header />
-          <Route exact path="/" component={Home} />
+          {/* if we have userName, we are authed */}
+          <Header isAuthed={userName}/>
+          <Route exact path="/" render={() => (
+            <Home name={userName} />
+          )} />
           <Route exact path="/my-books" component={Library} />
           <Route exact path='/recommendations' component={Recommendations} />
         </div>
