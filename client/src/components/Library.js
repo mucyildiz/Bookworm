f37c54bb0242d3cc8ca6fea503b1e7a755586988
Alperen Yildiz <alperenyildiz21@gmail.com>
2021-06-16 19:13:07 -0400

    removed unnecessary code

--- client/src/components/Library.js
+++ client/src/components/Library.js
@@ -1,19 +1,12 @@
 import './Library.css';
 import Search from './Search';
-import axios from 'axios';
 
 const Library = (props) => {
   const googleBooksAPIKey = props.apiKey;
 
-  const test = async () => {
-    const response = await axios.get(`https://www.googleapis.com/books/v1/volumes?q=harry-potter&key=AIzaSyDYLvzacyDGmqhxYbqSFBLGa1F3OYY_cbU`);
-    const data = await response.data;
-    console.log(data.items);
-  }
-
   return (
     <div id='library-container'>
-      <Search id='search-bar' key={googleBooksAPIKey} />
+      <Search id='search-bar' googleAPIKey={googleBooksAPIKey} />
     </div>
   )
 }
