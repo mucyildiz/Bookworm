17687be6c00ee79d1fb001974f913980cf629275
Alperen Yildiz <alperenyildiz21@gmail.com>
2021-06-16 19:14:31 -0400

    renamed variable

--- controllers/library-ctrl.js
+++ controllers/library-ctrl.js
@@ -2,9 +2,9 @@ const mongoose = require('mongoose');
 const User = mongoose.model('users');
 
 const addBook = async (req, res) => {
-  const bookTitle = req.body.title;
+  const bookInfo = req.body.bookInfo;
   const user = await User.findById(req.user._id).exec();
-  user.library.push(bookTitle);
+  user.library.push(bookInfo);
   user.save();
 };
 
