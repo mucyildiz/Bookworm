b782320d36884a0f7f8c2347518e1bd013816ccf
Alperen Yildiz <alperenyildiz21@gmail.com>
2021-06-16 02:08:04 -0400

    added and styled home screen for logged out user

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,42 +1,40 @@
 import './Home.css';
 import { Link } from 'react-router-dom';
-import { useState, useEffect } from 'react';
-import axios from 'axios';
 
-function Home() {
-  const [userName, setUserName] = useState('');
-
-  useEffect(() => {
-    const fetchData = async () => {
-      const response = await axios.get('/api/getuser');
-      const userInfo = await response.data;
-      setUserName(userInfo.firstName);
-    };
-    fetchData();
-  }, [])
-
-  return (
-    <div id='home-container'>
-      <div id='greeting'>
-        <div>Welcome {userName}!</div>
-        <div>You have x books in your library.</div>
-      </div>
-      <div id='options'>
-        <Link to="/my-books">
-          <div className='option'>
-            <img className='option-img' src='/images/bookreader2.svg' alt='library'/>
-            <div className='option-text'>Go to library</div>
+function Home(props) {
+  const renderHome = () => {
+    if(props.name) {
+      return (
+        <div id='home-container'>
+          <div id='greeting'>
+            <div>Welcome {props.name}!</div>
+            <div>You have x books in your library.</div>
           </div>
-        </Link>
-        <Link to='/recommendations'>
-          <div className='option'>
-            <img className='option-img' src='/images/bookreader1.svg' alt='find books' />
-            <div className='option-text'>Find Books</div>
+          <div id='options'>
+            <Link to="/my-books">
+              <div className='option'>
+                <img className='option-img' src='/images/bookreader2.svg' alt='library'/>
+                <div className='option-text'>Go to library</div>
+              </div>
+            </Link>
+            <Link to='/recommendations'>
+              <div className='option'>
+                <img className='option-img' src='/images/bookreader1.svg' alt='find books' />
+                <div className='option-text'>Find Books</div>
+              </div>
+            </Link>
           </div>
-        </Link>
+        </div>
+      )
+    }
+    return (
+      <div id='home-container'>
+        <h2 id='logged-out-msg'>To log in and start finding new books, simply press the button at the top right and sign in with your Google account!</h2>
+        <img id='guy-sitting-img' src='/images/bookreader3.svg' alt='man sitting on large books reading a much smaller book' />
       </div>
-    </div>
-  )
+    )
+  }
+  return renderHome();
 }
 
 export default Home;
