cb461c4b15e760bd622bde886048f387cae9c13f
Alperen Yildiz <alperenyildiz21@gmail.com>
2021-06-30 15:33:25 -0400

    added button to add book to library

--- client/src/components/BookResult.js
+++ client/src/components/BookResult.js
@@ -1,13 +1,32 @@
 import './BookResult.css';
+import axios from 'axios';
+//  '/api/addbook'
 
 const BookResult = props => {
 
+  const bookInfo = {bookInfo: {
+    imgUrl: props.imgUrl,
+    title: props.title,
+    author: props.author,
+  }}
+
+  const addBookToLibrary = async () => {
+    await axios.put('/api/addbook', bookInfo);
+  }
+
   return (
     <div className='book-result-container'>
-      <img className='book-img' src={props.imgUrl} alt='' />
-      <div className='book-info'>
-        <div className='title'>{props.title}</div>
-        <div>{props.author}</div>
+      <div className='left'>
+        <img className='book-img' src={props.imgUrl} alt='' />
+        <div className='book-info'>
+          <div className='title'>{props.title}</div>
+          <div>{props.author}</div>
+        </div>
+      </div>
+      <div>
+        {props.isSearchResult &&
+          <div onClick={addBookToLibrary} className='add-book-button'>Add Book to Library</div>
+        }
       </div>
     </div>
   )
