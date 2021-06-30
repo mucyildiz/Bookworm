99a0dfbcd25181983dec25e658f10e9e98c72189
Alperen Yildiz <alperenyildiz21@gmail.com>
2021-06-30 18:20:12 -0400

    added removeBook function

--- controllers/library-ctrl.js
+++ controllers/library-ctrl.js
@@ -2,12 +2,21 @@ const mongoose = require('mongoose');
 const User = mongoose.model('users');
 
 const addBook = async (req, res) => {
-  const bookInfo = req.body.bookInfo;
   const user = await User.findById(req.user._id).exec();
+  const bookInfo = req.body.bookInfo;
   user.library.push(bookInfo);
   user.save();
 };
 
+const removeBook = async (req, res) => {
+  const user = await User.findById(req.user._id).exec();
+  const bookInfo = req.body.bookInfo;
+  const id = bookInfo.bookId;
+  const newLibrary = user.library.filter(book => book.bookId !== id);
+  user.library = newLibrary;
+  user.save();
+}
+
 module.exports = {
-  addBook,
+  addBook, removeBook
 }
