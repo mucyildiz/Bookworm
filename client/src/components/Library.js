edbb3803ce3841c8ab72bd84277269cfd4902ad9
Alperen Yildiz <alperenyildiz21@gmail.com>
2021-06-30 18:18:14 -0400

    library retrieves user library from database and displays

--- client/src/components/Library.js
+++ client/src/components/Library.js
@@ -1,12 +1,41 @@
 import './Library.css';
-import Search from './Search';
+import { useState, useEffect } from 'react';
+import axios from 'axios';
+import BookResult from './BookResult';
 
 const Library = (props) => {
-  const googleBooksAPIKey = props.apiKey;
+  const [library, setLibrary] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const res = await axios.get('/api/getlibrary');
+      const userLibrary = res.data;
+      setLibrary(userLibrary);
+    }
+    fetchData();
+  }, [])
+
 
   return (
     <div id='library-container'>
-      <Search id='search-bar' googleAPIKey={googleBooksAPIKey} />
+      <div className="library-header">
+        <h1>Your Library</h1>
+      </div>
+      <ul className='library-books'>
+        {library.map(book => (
+          <li key={book.bookId}>
+            <BookResult 
+            bookId={book.bookId}
+            title={book.title}
+            imgUrl={book.imgUrl ? book.imgUrl : '/images/placeholderbook.svg'}
+            author={book.author ? book.author : ''}
+            description={book.description}
+            isInLibrary={true}
+            />
+          </li>
+        ))
+      }
+      </ul>
     </div>
   )
 }
