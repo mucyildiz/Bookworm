7a3087f51226b03d50c5d782e9f8d8c004cdc3f8
Alperen Yildiz <alperenyildiz21@gmail.com>
2021-06-30 12:07:56 -0400

    removed unnecessary code and changed architecture of component

--- client/src/components/BookResult.js
+++ client/src/components/BookResult.js
@@ -1,30 +1,14 @@
-import axios from 'axios';
-import { useEffect, useState } from 'react';
 import './BookResult.css';
 
 const BookResult = props => {
 
-  const addBookToLibrary = async () => {
-    const bookInfo = {
-      id: props.bookId,
-      title: props.title,
-      subtitle: props.subtitle,
-      imgUrl: props.imgUrl,
-      author: props.author
-    };
-    console.log('here');
-    await axios.put('/api/addbook', { bookInfo });
-  }
-
   return (
     <div className='book-result-container'>
-      <img src={props.imgUrl} className='book-img' alt=''/>
+      <img className='book-img' src={props.imgUrl} alt='' />
       <div className='book-info'>
-        <div>{props.title}</div>
-        <div>{props.subtitle && props.subtitle}</div>
+        <div className='title'>{props.title}</div>
         <div>{props.author}</div>
       </div>
-      <button onClick={addBookToLibrary} className='add-book btn'>Add Book to Library</button>
     </div>
   )
 }
