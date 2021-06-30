5dd3e485783a02455515315bca1296b00413375f
Alperen Yildiz <alperenyildiz21@gmail.com>
2021-06-30 18:21:04 -0400

    added api endpoints for getting user library and removing book from library

--- routes/libraryRoutes.js
+++ routes/libraryRoutes.js
@@ -1,5 +1,16 @@
+const { json } = require('express');
+const mongoose = require('mongoose');
 const LibraryCtrl = require('../controllers/library-ctrl');
+const User = mongoose.model('users');
 
 module.exports = app => {
   app.put('/api/addbook', LibraryCtrl.addBook)
+
+  app.put('/api/removebook', LibraryCtrl.removeBook)
+
+  app.get('/api/getlibrary', async (req, res) => {
+    const user = await User.findById(req.user._id).exec();
+    const library = user.library;
+    res.json(library);
+  })
 }
