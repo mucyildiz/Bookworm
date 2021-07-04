da60d47247af612d6fac5c60819f711f1aef0744
Alperen Yildiz <alperenyildiz21@gmail.com>
2021-07-04 15:25:31 -0400

    added heroku env vars

---
+++ config/prod.js
@@ -0,0 +1,7 @@
+module.exports = {
+  mongoURI: process.env.MONGO_URI,
+  googleClientID: process.env.GOOGLE_CLIENT_ID,
+  googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
+  googleAPIKey: process.env.GOOGLE_API_KEY,
+  cookieKey: process.env.COOKIE_KEY,
+}
